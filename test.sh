5d5ffdba92eaf681267b66c944898e97a447bb45
Andrey Romanov <aromanov.app@gmail.com>
2019-03-07 21:52:02 +0300

    add extra echo in test.sh

--- test.sh
+++ test.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-# Check master branch
+
+echo "Testing ""$TRAVIS_BRANCH"" branch"
+
+# Check 'master' branch
 if [ "$TRAVIS_BRANCH" == "" ]; then
 	echo "We don't have tests for master branch"
 	exit 0
 fi
 
+# Check 'play-travis' branch
 if [ "$TRAVIS_BRANCH" == "play-travis" ]; then
 	cd play-travis
+	echo "Run python test.py in play-travis directory"
 	RESULT="$(python test.py)"
 	VALID="$("${RESULT}" | grep -o 'failing')"
 	if [ "$VALID" != "FAILED" ]; then
